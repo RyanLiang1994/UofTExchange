d7e018e7526e125d9fdd38cc54309c399bc1ce41
Ryan Ruoshui Yan <ryan.yan@mail.utoronto.ca>
2016-07-11 23:55:26 -0400

    Add CSS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#signup').hide();
+    $('#signout').hide();
+    $('#myprofile').hide();
+    $('#search').hide();
+    $('#recommand').hide();
+    $('#help').hide();
+    $('#feedback').hide();
+});
