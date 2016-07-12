0ac627052cc21a87393eae81f5e6f995a2e6a5fc
Ryan Ruoshui Yan <ryan.yan@mail.utoronto.ca>
2016-07-12 00:14:51 -0400

    Add nav bar functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
 $(document).ready(function() {
-    $('#signup').hide();
-    $('#signout').hide();
-    $('#myprofile').hide();
-    $('#search').hide();
-    $('#recommand').hide();
-    $('#help').hide();
-    $('#feedback').hide();
+    // $('#signup').hide();
+    // $('#signout').hide();
+    // $('#myprofile').hide();
+    // $('#search').hide();
+    // $('#recommend').hide();
+    // $('#help').hide();
+    // $('#feedback').hide();
+    $('section').hide();
+    $('#home').show();
+});
+
+$('#btnHome').click(function() {
+    $('section').hide();
+    $('#home').show();
+});
+
+$('#btnSignup').click(function() {
+    $('section').hide();
+    $('#signup').show();
+});
+
+$('#btnSignout').click(function() {
+    $('section').hide();
+    $('#signout').show();
+});
+
+$('#btnMyprofile').click(function() {
+    $('section').hide();
+    $('#myprofile').show();
+});
+
+$('#btnSearch').click(function() {
+    $('section').hide();
+    $('#search').show();
+});
+
+$('#btnRecommend').click(function() {
+    $('section').hide();
+    $('#recommend').show();
+});
+
+$('#btnHelp').click(function() {
+    $('section').hide();
+    $('#help').show();
+});
+
+$('#btnFeedback').click(function() {
+    $('section').hide();
+    $('#feedback').show();
 });
