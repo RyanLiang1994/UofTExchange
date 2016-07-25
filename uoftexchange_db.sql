f316762e1e5475433dc068d017b6957b9c806bf3
Sam Zhang <samzhang@Sams-MacBook-Pro.local>
2016-07-24 20:57:11 -0400

    changed username to email.

--- uoftexchange_db.sql
+++ uoftexchange_db.sql
@@ -1,8 +1,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users {
-	username VARCHAR(32) PRIMARY KEY,
+	email VARCHAR(64) PRIMARY KEY,
 	password VARCHAR(32) CHECK (LEN(password) BETWEEN 6 AND 32),
-	email VARCHAR(64),
 	phone INT,
 	year_of_study INT CHECK (year_of_study BETWEEN 1 AND 4),
 	major VARCHAR(32),
@@ -29,19 +28,19 @@ CREATE TABLE courses {
 
 DROP TABLE IF EXISTS offers_book;
 CREATE TABLE offers_book {
-	username VARCHAR(32) FOREIGN KEY REFERENCES users(username) ON DELETE CASCADE,
+	email VARCHAR(32) FOREIGN KEY REFERENCES users(email) ON DELETE CASCADE,
 	title VARCHAR(64),
 	author VARCHAR(32),
-	PRIMARY KEY (username, title, author),
+	PRIMARY KEY (email, title, author),
 	FOREIGN KEY (title, author) REFERENCES books(title, author) ON DELETE CASCADE
 }
 
 DROP TABLE IF EXISTS offers_course;
 CREATE TABLE offers_course {
-	username VARCHAR(32) FOREIGN KEY REFERENCES users(username) ON DELETE CASCADE,
+	email VARCHAR(32) FOREIGN KEY REFERENCES users(email) ON DELETE CASCADE,
 	dept CHAR(3),
 	num INT,
-	PRIMARY KEY (username, dept, num),
+	PRIMARY KEY (email, dept, num),
 	FOREIGN KEY (dept, num) REFERENCES courses(dept, num) ON DELETE CASCADE
 }
 
