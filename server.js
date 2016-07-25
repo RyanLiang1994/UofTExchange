5f704feef2b3127e7882e5671fa7b9e6b32236c9
Sam Zhang <samzhang@inside-212-38.wireless.utoronto.ca>
2016-07-25 15:47:24 -0400

    blah.

--- server.js
+++ server.js
@@ -4,18 +4,14 @@ var express = require("express");
 var app = express();
 var expressValidator = require("express-validator");
 var bodyParser = require('body-parser');
+var expressSession = require("express-session");
+
 
 /*
 	this is a list of url to save url which we
 	might receive request later
 */
-var url_lst = [
-		'/',
-		'/index.html',
-		'/scripts/script.js',
-		'/scripts/jquery-2.2.4.min.js',
-		'/css/style.css'
-];
+var url_list = ['/', '/index.html'];
 
 
 /*
@@ -31,8 +27,8 @@ function startServer() {
       	res.setHeader('Content-Type', 'text/html');
 		switch (req.url) {
 			// case of url '/' and '/a3.html'
-			case url_lst[0]:
-			case url_lst[1]:
+			case url_list[0]:
+			case url_list[1]:
 				var content = fs.readFileSync('./index.html', 'utf8');
 				res.writeHead(200, {"Content-Type": "text/html"});
 				res.end(content);
@@ -40,8 +36,8 @@ function startServer() {
 
 			// case of url '/assets/scripts/script.js' and
 			// '/assets/scripts/jquery-2.2.4.min.js',
-			case url_lst[2]:
-			case url_lst[3]:
+			case url_list[2]:
+			case url_list[3]:
 				var content = fs.readFileSync('.' + req.url, 'utf8');
 				res.writeHead(200, {"Content-Type": "text/javascript"});
 				res.end(content);
@@ -49,7 +45,7 @@ function startServer() {
 				break;
 
 			// case of url '/assets/css/style.css'
-			case url_lst[4]:
+			case url_list[4]:
 				var content = fs.readFileSync('.' + req.url, 'utf8');
 				res.writeHead(200, {"Content-Type": "text/css"});
 				res.end(content);
@@ -69,11 +65,11 @@ console.log('Server running at http://localhost:3000/');
 */
 
 app.use(express.static(__dirname + '/assets'));
-app.engine('.html', require('ejs').__express);
-app.set('views', __dirname);
-app.set('view engine', 'html');
+// app.engine('.html', require('ejs').__express);
+// app.set('views', __dirname);
+// app.set('view engine', 'html');
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
@@ -100,11 +96,12 @@ app.use(expressValidator({
 }));
 
 
-app.get(url_lst[0] || url_lst[1], function(req, res) {
-    res.render('index', {  // Note that .html is assumed.
-        "errors": ''
-    });
-	console.log('hi');
+app.get(url_list[0] || url_list[1], function(req, res) {
+ //    res.render('index', {  // Note that .html is assumed.
+ //        "errors": ''
+ //    });
+	// console.log('hi');
+	res.sendFile(__dirname + '/index.html');
 });
 
 
@@ -125,6 +122,7 @@ app.post('/signup', function(req, res)
     {
         var msgs = { "errors": {} };
 
+
         if ( mappedErrors.uname )
             msgs.errors.error_uname = mappedErrors.uname.msg;
 
@@ -132,42 +130,12 @@ app.post('/signup', function(req, res)
             msgs.errors.error_pword = mappedErrors.pword.msg;
 
 
-        res.render('index', msgs);
+        res.sendFile(__dirname + '/index.html');
+        // res.render('index', msgs);
 
     } else {
 		//submit the data to database
 		console.log("signup");
-	}
-});
-
-
-app.post('/signin', function(req, res)
-{
-	req.assert('username', 'Username is required').notEmpty();
-	req.assert('password', 'Password is required').notEmpty();
-
-	req.checkBody('username', 'Username is not valid').isUserName();
-	req.checkBody('password', 'Password is not valid').isPassword();
-
-	var err = req.validationErrors();
-    var mappedErrors = req.validationErrors(true);
-
-    if (err) // If errors exist, send them back to the form:
-    {
-        var msgs = { "errors": {} };
-
-        if ( mappedErrors.username )
-            msgs.errors.error_username = mappedErrors.username.msg;
-
-        if ( mappedErrors.password )
-            msgs.errors.error_password = mappedErrors.password.msg;
-
-
-        res.render('index', msgs);
-
-    } else {
-		//submit the data to database
-		console.log("signin");
 	}
 });
 
