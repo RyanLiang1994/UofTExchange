871cd80ba9b05a808279a0796a71354d557b24d5
Ryan Ruoshui Yan <ryan.yan@mail.utoronto.ca>
2016-07-25 15:45:50 -0400

    Add google sign in

--- uoftexchange_db.sql
+++ uoftexchange_db.sql
@@ -1,51 +1,51 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE users {
+CREATE TABLE users (
 	email VARCHAR(64) PRIMARY KEY,
-	password VARCHAR(32) CHECK (LEN(password) BETWEEN 6 AND 32),
+	password VARCHAR(32) CHECK (LENGTH(password) BETWEEN 8 AND 32),
 	phone INT,
 	year_of_study INT CHECK (year_of_study BETWEEN 1 AND 4),
 	major VARCHAR(32),
-	is_admin INT NOT NULL CHECK （is_admin BETWEEN 0 AND 1）
-}
+	is_admin INT NOT NULL CHECK (is_admin BETWEEN 0 AND 1)
+);
 
 DROP TABLE IF EXISTS books;
-CREATE TABLE books {
+CREATE TABLE books (
 	title VARCHAR(64),
 	author VARCHAR(32),
 	publisher VARCHAR(64),
 	isbn INT,
 	PRIMARY KEY (title, author)
-}
+);
 
 DROP TABLE IF EXISTS courses;
-CREATE TABLE courses {
-	dept CHAR(3) CHECK (LEN(dept) = 3),
+CREATE TABLE courses (
+	dept CHAR(3) CHECK (LENGTH(dept) = 3),
 	num INT,
 	title VARCHAR(64),
 	sect VARCHAR(16),
-	PRIMARY KEY (dept, code)
-}
+	PRIMARY KEY (dept, num)
+);
 
 DROP TABLE IF EXISTS offers_book;
-CREATE TABLE offers_book {
-	email VARCHAR(32) FOREIGN KEY REFERENCES users(email) ON DELETE CASCADE,
+CREATE TABLE offers_book (
+	email VARCHAR(32) REFERENCES users(email) ON DELETE CASCADE,
 	title VARCHAR(64),
 	author VARCHAR(32),
 	PRIMARY KEY (email, title, author),
 	FOREIGN KEY (title, author) REFERENCES books(title, author) ON DELETE CASCADE
-}
+);
 
 DROP TABLE IF EXISTS offers_course;
-CREATE TABLE offers_course {
-	email VARCHAR(32) FOREIGN KEY REFERENCES users(email) ON DELETE CASCADE,
+CREATE TABLE offers_course (
+	email VARCHAR(32) REFERENCES users(email) ON DELETE CASCADE,
 	dept CHAR(3),
 	num INT,
 	PRIMARY KEY (email, dept, num),
 	FOREIGN KEY (dept, num) REFERENCES courses(dept, num) ON DELETE CASCADE
-}
+);
 
 DROP TABLE IF EXISTS course_textbook;
-CREATE TABLE course_textbook {
+CREATE TABLE course_textbook (
 	dept CHAR(3),
 	num INT,
 	title VARCHAR(64),
@@ -53,4 +53,21 @@ CREATE TABLE course_textbook {
 	PRIMARY KEY (dept, num, title, author),
 	FOREIGN KEY (dept, num) REFERENCES courses(dept, num) ON DELETE CASCADE,
 	FOREIGN KEY (title, author) REFERENCES books(title, author) ON DELETE CASCADE
-}
+);
+
+DROP TABLE IF EXISTS follows;
+CREATE TABLE follows (
+	user1 VARCHAR(64) REFERENCES users(email) ON DELETE CASCADE,
+	user2 VARCHAR(64) REFERENCES users(email) ON DELETE CASCADE,
+	PRIMARY KEY (user1, user2),
+	CHECK(user1 <> user2)
+);
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+	user1 VARCHAR(64) REFERENCES users(email) ON DELETE CASCADE,
+	user2 VARCHAR(64) REFERENCES users(email) ON DELETE CASCADE,
+	message VARCHAR(128), 
+	PRIMARY KEY (user1, user2),
+	CHECK(user1 <> user2)
+);
