ed9aa4fb36e622b72548c71e6aaa98ee97c4523a
Ryan Leung <Ryan Leung>
2016-07-29 17:51:30 -0400

    server

--- server.js
+++ server.js
@@ -752,7 +752,7 @@ function emptyStringToNull(value) {
         return value;
     }
 }
-var server = app.listen(3000, function()
+var server = app.listen(process.env.PORT || 3000, function()
 {
   var port = server.address().port;
   console.log('Running on 127.0.0.1:%s', port);
